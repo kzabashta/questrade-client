9f68368ef25f3b0684eae583c3bef0bc9383a108
kzabashta <kosta.zabashta@gmail.com>
2019-02-23 18:19:01 -0500

    more changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+import json
 
 import asyncio
+from influxdb import InfluxDBClient
 
 from client import QTClient
 
@@ -10,18 +12,40 @@ logging.basicConfig(level=logging.INFO,
                                   logging.StreamHandler()])
 
 if __name__ == '__main__':
+    logging.info("Reading refresh token")
     f = open("/mnt/refresh_token.txt", "r")
     refresh_token = f.read()
 
+    logging.info("Connecting to Questrdate")
     client = QTClient(refresh_token)
+    logging.info("Connecting to InfluxDB")
+    influx_client = InfluxDBClient(host='influxdb', database='market')
+    logging.info("OK")
+    async def scraper(ticks):
+        ticks = json.loads(ticks)
+        if 'quotes' in ticks:
+            for tick in ticks['quotes']:
+                try:
+                    json_body = [
+                        {
+                            "measurement": "market",
+                            "tags": {
+                                "symbol": tick['symbol']
+                            },
+                            "fields": {
+                                "bidPrice": tick['bidPrice'],
+                                "bidSize": tick['bidSize'],
+                                "askPrice": tick['askPrice'],
+                                "askSize": tick['askSize'],
+                                "lastTradePrice": tick['lastTradePrice'],
+                                "lastTradeSize": tick['lastTradeSize'],
+                                "volume": tick['volume'],
+                                "VWAP": tick['VWAP']
+                            }
+                        }
+                    ]
+                    influx_client.write_points(json_body)
+                except Exception:
+                    logging.exception("message")
 
-    account_id = client.get_account()['accounts'][0]['number']
-    logging.info('Account Number: %s' % account_id)
-
-    candles = client.get_candles('8049', '2017-10-01T00:00:00-05:00', '2018-10-01T00:00:00-05:00', 'OneDay')
-    logging.info(candles)
-
-    async def printer(message):
-        logging.info('From main' + message)
-
-    asyncio.get_event_loop().run_until_complete(client.subscribe(['8049'], printer))
+    asyncio.get_event_loop().run_until_complete(client.subscribe(['8049', '9292'], scraper))
